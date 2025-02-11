15e93b39083043e3628d398224c1cc0997d65388
Jisun696 <istiakkashem35@gmail.com>
2025-02-11 21:16:37 +0600

    Make the card component ui and function to get all filtered products

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navigation from "./components/Navigation/Navigation";
 import data from "./db/data";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Recommended from "./components/Recommended/Recommended";
+import Card from "./components/UI/Card";
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
@@ -28,6 +29,30 @@ function App() {
     setSelectedCategory(event.target.value);
     console.log(event.target.value);
   };
+  console.log("render selectedCategory ", selectedCategory);
+  console.log("render query ", query);
+
+  const filteredProducts = (products, selected, query) => {
+    let filteredProducts = products;
+
+    // filter input items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // apply selected filters
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) => category === selected || color === selected || company === selected || newPrice === selected || title === selected
+      );
+    }
+
+    return filteredProducts.map(({ img, title, star, reviews, prevPrice, newPrice }) => (
+      <Card img={img} title={title} star={star} reviews={reviews} prevPrice={prevPrice} newPrice={newPrice} />
+    ));
+  };
+
+  const result = filteredProducts(data, selectedCategory, query);
 
   return (
     <>
